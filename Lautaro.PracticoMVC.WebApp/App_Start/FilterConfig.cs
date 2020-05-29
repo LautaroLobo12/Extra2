9b5243a3f0ae529db27e1c72838d66e97940b2bd
Lautaro Jordan Lobo Ravarotto <43019989+LautaroLobo12@users.noreply.github.com>
2020-05-29 14:53:39 -0300

    uploading PracticoMVC.WebApp

---
+++ Lautaro.PracticoMVC.WebApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Lautaro.PracticoMVC.WebApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
