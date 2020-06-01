92dcdf7e2a684d2d64bee654b50e7e6dc6bc3dfb
Lautaro Jordan Lobo Ravarotto <43019989+LautaroLobo12@users.noreply.github.com>
2020-06-01 11:39:48 -0300

    update clientes.cs

---
+++ Lautaro.PracticoMVC.AccesoDatos/Clientes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using Dapper;
+
+
+namespace Lautaro.PracticoMVC.AccesoDatos {
+    public class Clientes {
+        string cadenaConexion = Conexiones.ObtenerCadenaConexion();
+        public int Test() {
+            int filasAfectadas = 0;
+
+            StringBuilder consultaSQL = new StringBuilder();
+
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+
+            consultaSQL.Append(" ");
+            consultaSQL.Append(" ");
+            consultaSQL.Append(" ");
+            consultaSQL.Append(" ");
+            consultaSQL.Append(" ");
+            consultaSQL.Append(" ");
+            consultaSQL.Append(" ");
+
+
+            SqlTransaction transaccion = conexion.BeginTransaction();
+
+            try {
+                conexion.Open();
+                filasAfectadas = conexion.Execute(consultaSQL.ToString(),
+                   new
+                   {
+                       // params
+                   });
+                transaccion.Commit();
+            }
+            catch (Exception ex) {
+                transaccion.Rollback();
+                filasAfectadas = 0;
+            }
+            finally {
+                conexion.Close();
+            }
+            return filasAfectadas;
+        }
+    }
+}
